441c78ba08c5652c39fdee5347ec258189947580
Afis KOUSSE <mahoutinafis@gmail.com>
2022-08-19 05:10:16 +0100

    React props

--- src/App.js
+++ src/App.js
@@ -4,34 +4,37 @@ function getTitle(title) {
   return title;
 }
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'Angular',
-    url: 'https://angular.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+
 
 const App = ()=> {
+
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+        {
+            title: 'Angular',
+            url: 'https://angular.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
+
     const handleChange = event =>{
         console.log(event);
     }
@@ -42,14 +45,14 @@ const App = ()=> {
             <input id="search" type="text" onChange={handleChange}/>
 
             <hr />
-            <List />
+            <List list={stories} />
         </div>
     );
 }
 
 
-const List = ()=>
-   list.map((item) =>
+const List =  props =>
+    props.list.map((item) =>
      (
         <div key={item.objectID}>
         <span>
