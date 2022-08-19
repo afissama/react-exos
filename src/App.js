7bc6d056e804c25eec2f54ae1016bb0092df3065
Afis KOUSSE <mahoutinafis@gmail.com>
2022-08-19 11:05:41 +0100

    React States

--- src/App.js
+++ src/App.js
@@ -34,16 +34,18 @@ const App = ()=> {
             objectID: 1,
         },
     ];
-
+    const [searchTerm, setSearchTerm] = React.useState('');
     const handleChange = event =>{
-        console.log(event);
+        setSearchTerm(event.target.value)
     }
     return (
         <div>
             <h1>{getTitle("React")}</h1>
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" onChange={handleChange}/>
-
+            <p>
+                Searching for <strong>{searchTerm}</strong>
+            </p>
             <hr />
             <List list={stories} />
         </div>
