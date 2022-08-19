e802b8e98b60e37736c86c289d55bc12458a915a
Afis KOUSSE <mahoutinafis@gmail.com>
2022-08-19 04:22:37 +0100

    React DOM

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <h1>Hello React world</h1>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
